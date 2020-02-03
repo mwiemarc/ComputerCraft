362bd82e198f877215a8723b46525a5e6b596927
mwiemarc <mwiemarc@gmail.com>
2020-02-04 00:23:22 +0100

    added program: dl

---
+++ dl.lua
@@ -0,0 +1,15 @@
+local args = {...}
+
+if #args ~= 2 then
+    print('usage: dl <url> <filename>')
+end
+
+local req = http.get(args[1])
+local res = req.readAll()
+req.close()
+
+local f = fs.open(args[2], 'w')
+f.write(res)
+f.close()
+
+print('download complete')
